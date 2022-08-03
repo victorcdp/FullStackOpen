a47e6fb04e7da181a82d5ba3959fbf948ca94c85
João Victor Cordeiro <joao.cordeiro@liferay.com>
2022-08-03 15:23:39 -0300

    1.7 unicafe step2 and 1.8 unicafe step3

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,31 +6,51 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const Statistics = ({ text, value }) => (
+const Statistics = ({ value }) => (
   <div>
-    {text} {value}
+    <div>
+      good {value.good}
+    </div>
+    <div>
+      neutral {value.neutral}
+    </div>
+    <div>
+      bad {value.bad}
+    </div>
+    <div>
+      all {value.total}
+    </div>
+    <div>
+      average {(value.good + value.bad * -1) / value.total}
+    </div>
+    <div>
+      positive {(value.good / value.total) * 100} %
+    </div>
   </div>
 )
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [clicks, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0, total: 0
+  })
 
-  const incrementGood = () => setGood(good + 1)
-  const incrementNeutral = () => setNeutral(neutral + 1)
-  const incrementBad = () => setBad(bad + 1)
+  const handleGoodClick = () => 
+    setClicks({ ...clicks, good: clicks.good + 1, total: clicks.total + 1})
+
+  const handleNeutralClick = () => 
+    setClicks({ ...clicks, neutral: clicks.neutral + 1, total: clicks.total + 1})
+
+  const handleBadClick = () => 
+    setClicks({ ...clicks, bad: clicks.bad + 1, total: clicks.total + 1})
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={incrementGood} text="good" />
-      <Button handleClick={incrementNeutral} text="neutral" />
-      <Button handleClick={incrementBad} text="bad" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <h1>statistics</h1>
-      <Statistics text="good" value={good} />
-      <Statistics text="neutral" value={neutral} />
-      <Statistics text="bad" value={bad} />
+      <Statistics value={clicks} />
 </div>
   )
 }
