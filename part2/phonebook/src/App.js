a397725b8c2a4884442c41f58176cc46eeb40996
João Victor Cordeiro <joao.cordeiro@liferay.com>
2022-08-10 17:58:51 -0300

    2.6 phonebook step 1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const Person = ({ person }) => {
+  return(
+    <li>{person.name}</li>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      date: new Date().toISOString(),
+      id: persons.length + 1
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: 
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <Person key={person.name} person={person} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
