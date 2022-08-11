69cab27ae91c6d7ec2a298dd2cc52bf9df948229
João Victor Cordeiro <joao.cordeiro@liferay.com>
2022-08-11 13:36:52 -0300

    2.8 phonebook step 3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,25 @@ import { useState } from 'react'
 
 const Person = ({ person }) => {
   return(
-    <li>{person.name}</li>
+    <li>{person.name} {person.number}</li>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567',
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const isRepeated = () => {
@@ -35,12 +42,14 @@ const App = () => {
     else{
       const nameObject = {
         name: newName,
+        number: newNumber,
         date: new Date().toISOString(),
         id: persons.length + 1
       }
 
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -54,8 +63,15 @@ const App = () => {
             value={newName}
             onChange={handleNameChange}
           />
-          <button type="submit">add</button>
         </div>
+        <div>
+          number: 
+          <input 
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
       <ul>
