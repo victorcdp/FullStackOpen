bc47dfa61ecbc1626b2214165e9c870af016a50d
João Victor Cordeiro <joao.cordeiro@liferay.com>
2022-09-12 17:42:32 -0300

    2.11 phonebook step 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SearchFilter from './components/SearchFilter'
 import Persons from './components/Persons'
 import NewPersonForm from './components/NewPersonForm'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchFilter, setNewSearchFilter] = useState('')
@@ -18,6 +14,14 @@ const App = () => {
       person.name.toLowerCase().includes(searchFilter.toLowerCase())
     )
   )
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
